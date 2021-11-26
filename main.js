3a89ef1075c4d3b3fb9948d2c0810b0f55dfe6c3
viper <mahdiiging@gmail.com>
2021-11-26 03:19:48 +0100

    Add cookie reading and header referrer setting

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ var port = process.env.PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+    originWhitelist: ['http://127.0.0.1:5500','https://bad.example.com', 'http://bad.example.com'] // Allow all origins
+    //requireHeader: ['origin', 'x-requested-with'],
+   // removeHeaders: ['cookie', 'cookie2'],
+    //setHeaders :{"referer":"https://gogoplay1.com/embedplus?id=MTUxMzQ=&token=SJg72ThyS5RrSyvAhRyxMQ&expires=1637817460" }
 }).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+    console.log('Running CORS Anywhere on ' + "http://127.0.0.1:8080");
 });
